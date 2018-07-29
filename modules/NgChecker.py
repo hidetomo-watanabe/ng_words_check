67840f46c2ad9602c0e4f9907216bb4fb0ee8724
hidetomo-watanabe <attyamunn@gmail.com>
2018-07-29 02:58:32 +0000

    google IMEの文節単位で処理をし、orgの文節を返すように

--- modules/NgChecker.py
+++ modules/NgChecker.py
@@ -16,16 +16,13 @@ class NgChecker(object):
         f.close()
         return ng_words
 
-    def _trans_with_google_ime(self, text):
+    def _get_ime_trans(self, text):
         response = requests.get(
             'http://www.google.com/transliterate?langpair=ja-Hira|ja&text=%s' %
             text.strip())
-        transed = ''
-        for unit in json.loads(response.text):
-            transed += unit[1][0]
-        return transed
+        return json.loads(response.text)
 
-    def _get_parse(self, text):
+    def _get_morpheme_parse(self, text):
         org = self.mecab_obj.parse(text)
         result = {
             'noun': [],
@@ -37,12 +34,13 @@ class NgChecker(object):
                 break
             word, morphemes = line.split('\t')
             morpheme = morphemes.split(',')[0]
+            base_word = morphemes.split(',')[6]
             if morpheme == '名詞':
-                result['noun'].append(word)
+                result['noun'].append(base_word)
             elif morpheme == '動詞':
-                result['verb'].append(word)
+                result['verb'].append(base_word)
             else:
-                result['other'].append(word)
+                result['other'].append(base_word)
         return result
 
     def _trans_to_han_kata(self, text):
@@ -51,17 +49,21 @@ class NgChecker(object):
         return han_hira
 
     def get_ng_part(self, text):
-        mecab_result = self._get_parse(
-            self._trans_with_google_ime(text))
         check_result = []
         ng_words = self._get_ng_words()
         # 半角カタカナに統一
         ng_words_hankata = list(map(
             lambda x: self._trans_to_han_kata(x), ng_words))
-        for key in mecab_result.keys():
-            for word in mecab_result[key]:
-                if self._trans_to_han_kata(word) in ng_words_hankata:
-                    check_result.append(word)
+        ime_result = self._get_ime_trans(text)
+        # 文節ごとに処理
+        for unit in ime_result:
+            org = unit[0]
+            trans = unit[1][0]
+            mecab_result = self._get_morpheme_parse(trans)
+            for morpheme in mecab_result.keys():
+                for word in mecab_result[morpheme]:
+                    if self._trans_to_han_kata(word) in ng_words_hankata:
+                        check_result.append(org)
         return check_result
 
 
